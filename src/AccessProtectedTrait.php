9c01abc241c0dd6a20380ca299fdb4629bc85cc5
Xavier RODET <xavier.rodet@gmail.com>
2019-03-06 15:34:56 +0100

    init project

---
+++ src/AccessProtectedTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Snailweb\Tests;
+
+trait AccessProtectedTrait
+{
+    public function invokeMethod(&$object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+    public function getAttribute(&$object, $attributeName)
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $attribute = $reflection->getProperty($attributeName);
+        $attribute->setAccessible(true);
+
+        return $attribute->getValue($object);
+    }
+
+    public function setAttribute(&$object, $attributeName, $attributeValue)
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $attribute = $reflection->getProperty($attributeName);
+        $attribute->setAccessible(true);
+
+        $attribute->setValue($object, $attributeValue);
+    }
+}
---
+++ src/TestIteratorTrait.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Snailweb\Tests;
+
+trait TestIteratorTrait
+{
+    use AccessProtectedTrait;
+    private $iterator;
+
+    public function testConstruct()
+    {
+        $key = $this->getAttribute($this->iterator, 'key');
+
+        $this->assertSame(0, $key);
+    }
+
+    public function testIteratorCurrent()
+    {
+        $key = $this->getAttribute($this->iterator, 'key');
+        $signals = $this->getAttribute($this->iterator, 'signals');
+
+        $this->assertSame($signals[$key], $this->iterator->current());
+    }
+
+    public function testIteratorNext()
+    {
+        $oldKey = $this->getAttribute($this->iterator, 'key');
+        $this->iterator->next();
+        $newKey = $this->getAttribute($this->iterator, 'key');
+
+        $this->assertSame(($oldKey + 1), $newKey);
+    }
+
+    public function testIteratorKey()
+    {
+        $key = $this->getAttribute($this->iterator, 'key');
+
+        $this->assertSame($key, $this->iterator->key());
+    }
+
+    public function testIteratorRewind()
+    {
+        $this->iterator->rewind();
+        $key = $this->getAttribute($this->iterator, 'key');
+
+        $this->assertSame(0, $key);
+    }
+
+    public function testIteratorValidTrue()
+    {
+        $this->assertTrue($this->iterator->valid());
+    }
+}
